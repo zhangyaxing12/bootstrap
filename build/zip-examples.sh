6de57ec31367de2f6ddb83bfa6aaa773326ece29
XhmikosR <xhmikosr@gmail.com>
2020-04-01 15:13:43 +0300

    Remove integrity and crossorigin attributes from local files

--- build/zip-examples.sh
+++ build/zip-examples.sh
@@ -18,5 +18,7 @@ cp -r "_gh_pages/docs/$version_short/examples/" "$folder_name"
 cp -r "_gh_pages/docs/$version_short/dist/" "$folder_name"
 rm "$folder_name/index.html"
 find "$folder_name" -name "*.html" -exec sed -i "s#\"/docs/$version_short/#\"../#g" '{}' \;
+find "$folder_name" -name "*.html" -exec sed -i "s#\(<link href=\"../.*\) integrity=\".*#\1>#" '{}' \;
+find "$folder_name" -name "*.html" -exec sed -i "s#\(<script src=\"../.*\) integrity=\".*#\1></script>#" '{}' \;
 zip -r9 "$folder_name.zip" "$folder_name"
 rm -rf "$folder_name"
